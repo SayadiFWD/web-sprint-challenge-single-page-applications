93f264f24001293c7244464a3a308d4224127eb5
Ahmad Wali Sayadi <sayadi@Ahmads-MacBook-Pro.local>
2020-07-20 19:11:54 -0700

    home page is added to project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
-import Form from "./components/Form"
+import { Route, Link } from "react-router-dom";
+import Form from "./components/Form";
+import Home from "./components/Home";
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <Form/>
+      <Link to="/">Home</Link>
+      <Link to="/pizza">Shop</Link>
+
+      <Route exact path="/" component={Home} />
+
+      <Route path="/pizza/" component={Form} />
     </>
   );
 };
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <img
+        className="home-image"
+        src="https://source.unsplash.com/x00CzBt4Dfk"
+        alt=""
+      />
+    </div>
+  );
+}
