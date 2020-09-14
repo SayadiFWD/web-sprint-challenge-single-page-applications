477209a02c04019be7ae98677cbf6534a550e29f
Ahmad Wali Sayadi <sayadi@Ahmads-MacBook-Pro.local>
2020-09-13 18:55:19 -0700

    up date on form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState ,useEffect } from "react";
+import * as yup from "yup";
 
 function Form() {
   const [orders, setOrders] = useState({
@@ -11,8 +12,57 @@ function Form() {
     onion: "",
     special: "",
   });
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const [errors, setErrors] = useState({
+    name: "",
+    sizs: "",
+    pinapple: "",
+    olive: "",
+    sausage: "",
+    mushroom: "",
+    onion: "",
+    special: "",
+  });
+
+  //   Validation
+  const formSchema = yup.object().shape({
+    name: yup.string().required("Name is required"),
+    size: yup.string().required(),
+
+    pinapple: yup.boolean().oneOf([true], "Please Choose a topping"),
+    olive: yup.boolean().oneOf([true], "Please Choose a topping"),
+    sausage: yup.boolean().oneOf([true], "Please Choose a topping"),
+    mushroom: yup.boolean().oneOf([true], "Please Choose a topping"),
+    onion: yup.boolean().oneOf([true], "Please Choose a topping"),
+
+    spcial: yup.string(),
+  });
+
+  const validateChange = (e) => {
+    yup
+      .reach(formSchema, e.target.name)
+      .validate(e.target.value)
+      .the((valid) => {
+        setErrors({ ...errors, [e.target.name]: "" });
+      })
+      .catch((err) => setErrors({ ...errors, [e.target.name]: err.errors[0] }));
+  };
+
+  useEffect(() => {
+    formSchema.isValid(orders).then((valid) => {
+      setIsButtonDisabled(!valid);
+    });
+  }, [orders]);
+
+
+
+
+
+
 
   const inputChange = (e) => {
+    e.persist();
+    validateChange(0);
     setOrders({
       ...orders,
       [e.target.name]:
@@ -20,16 +70,32 @@ function Form() {
     });
   };
 
+  const formSubmit = (e) => {
+    e.preventDefaulte();
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmit}>
       <label htmlFor="nameInput">
         Name:
-        <input type="text" id="nameInput" name="name" />
+        <input
+          type="text"
+          id="nameInput"
+          name="name"
+          value={orders.name}
+          onChange={inputChange}
+        />
       </label>
       <br />
       {/* Size */}
       <label htmlFor="sizeInput">
-        <select id="sizeInput" name="size">
+        Select Pizza Size :<br />
+        <select
+          id="sizeInput"
+          name="size"
+          value={orders.size}
+          onChange={inputChange}
+        >
           <option></option>
           <option value="Small">Small</option>
           <option value="Medium">Medium</option>
@@ -100,7 +166,12 @@ function Form() {
       {/* special instructions */}
       <label htmlFor="specialInputs">
         Special Instructions :<br />
-        <textarea id="spicialInputs" name="special" />
+        <textarea
+          id="spicialInputs"
+          name="special"
+          value={orders.special}
+          onChange={inputChange}
+        />
       </label>
       <br />
       <button type="submit">Order</button>
